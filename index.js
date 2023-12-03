a95969947b31b18f4b569463036f48b19b22b4b2
ayush-122 <ayushsingh4045@gmail.com>
2023-12-03 21:39:21 +0530

    views

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port =8000;
 //use express Router
 app.use('/' ,require('./routes'));
 
+app.set('view-engine' , 'ejs');
+app.set('views' ,'./views');
+
 app.listen(port,function(err)
 {
     if(err)
