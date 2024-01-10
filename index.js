fabc039a6240440262eda3dad75eb5dd1169935a
ayush-122 <ayushsingh4045@gmail.com>
2024-01-10 23:23:18 +0530

    dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const app =express();
 const cookieParser =require('cookie-parser');
-const port =8000;
+const port =process.env.PORT ||8000;
 const expresslayouts = require('express-ejs-layouts');
 
 const db =require('./config/mongoose');
